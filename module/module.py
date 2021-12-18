e8c3406ed4eb093d256ca118dd66283bb312ca56
muhammadazri12 <muhammad.azri.f.s@gmail.com>
2021-12-18 15:37:17 +0700

    Module using

---
+++ module/module.py
@@ -0,0 +1,19 @@
+import matematika
+
+
+
+result = matematika.plus(10, 8)
+print (result)
+
+result = matematika.minus(10, 8)
+print (result)
+
+result = matematika.divide(10, 8)
+print (result)
+
+result = matematika.multiply(10, 8)
+print (result)
+
+from matematika import plus
+result = plus(10,10)
+print (result)
